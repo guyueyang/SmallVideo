472c6238d8106a8dae6264859e22ccf8b7b10f33
yangxioaping <yangxiaoping@westcoast.com.cn>
2017-04-10 16:20:18 +0800

    Initial commit

---
+++ smallvideolib/src/main/java/com/guyueyang/smallvideolib/adapter/camera/Listener/MediaRecorderListener.java
@@ -0,0 +1,11 @@
+package com.guyueyang.smallvideolib.adapter.camera.Listener;
+
+
+import com.guyueyang.smallvideolib.adapter.camera.util.MediaRecorder;
+
+/**
+ * Created by Administrator on 2017/4/7.
+ */
+public interface MediaRecorderListener {
+    void onEncodeComplete(MediaRecorder mediaRecorder);
+}
